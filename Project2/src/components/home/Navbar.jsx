f2bcf3382c4c732ed96e7754972db4be79043399
uttpal singh <uttpalsingh8@gmail.com>
2026-03-01 18:59:27 +0530

    initial commit

--- Project2/src/components/home/Navbar.jsx
+++ Project2/src/components/home/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { Hexagon } from "lucide-react";
 import { UserRound } from "lucide-react";
 import { BadgeAlert } from "lucide-react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Navbar = () => {
+const navRef = useRef(null)
+
+  const tl = gsap.timeline()
+  useGSAP(()=>{
+    tl.from(navRef.current,{
+      y:-40,
+      opacity:0,
+      duration:1,
+      delay:0.5
+    })
+  })
+
   return (
-    <div>
-      <div className="text-5xl flex flex-row justify-between p-5">
+    <div ref={navRef}>
+      <div className="logo text-5xl flex flex-row justify-between p-5">
         <Link to="/">
           <div className="uppercase flex items-center">
             <Hexagon size={60} />
